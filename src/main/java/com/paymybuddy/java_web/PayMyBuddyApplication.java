3ba0d46a9142bf032cb32d34370935e2fc327ed4
eeboulanger <eboulanger.pro@gmail.com>
2024-05-28 13:49:23 +0200

    Initial commit

---
+++ src/main/java/com/paymybuddy/java_web/PayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.java_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyApplication.class, args);
+	}
+
+}
